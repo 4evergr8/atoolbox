596fc2871b4ba5fd44e8f0afca10e0ace595535d
Joshhshh <137018415+4evergr8@users.noreply.github.com>
2025-04-02 15:59:50 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'screens/home_screen.dart';
 import 'widgets/bottom_nav_bar.dart';
 import 'util.dart'; // 假设 util.dart 包含 createTextTheme 函数
 import 'theme.dart'; // 假设 theme.dart 包含 MaterialTheme 类
@@ -12,6 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 设置状态栏为透明
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+
     final brightness = View.of(context).platformDispatcher.platformBrightness;
 
     // 创建自定义的主题
