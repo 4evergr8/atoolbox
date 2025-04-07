b20ce25fe07774d9768470974d3ac06d91e38f89
Joshhshh <137018415+4evergr8@users.noreply.github.com>
2025-04-07 11:30:25 +0800

    Update about_page.dart

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -23,7 +23,7 @@ class AboutPage extends StatelessWidget {
                 backgroundImage: const AssetImage('assets/profile.png'),
               ),
               const SizedBox(height: 20),
-              // 开发者名称
+
               Text(
                 '4evergr8',
                 style: Theme.of(context).textTheme.headlineMedium,
@@ -33,7 +33,7 @@ class AboutPage extends StatelessWidget {
               Text(
                 '此软件来源于无聊时的瞎想',
                 textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyMedium,
+                style: Theme.of(context).textTheme.bodyLarge,
               ),
               const SizedBox(height: 20),
 
@@ -61,9 +61,20 @@ class AboutPage extends StatelessWidget {
                 },
               ),
               ListTile(
+                leading: const Icon(Icons.code),
+                title: const Text('音乐分享'),
+                subtitle: const Text('我爱听的歌'),
+                onTap: () {
+                  launchUrl(
+                    Uri.parse('https://github.com/4evergr8/Music'),
+                    mode: LaunchMode.externalApplication,
+                  );
+                },
+              ),
+              ListTile(
                 leading: const Icon(Icons.web),
                 title: const Text('GithubPages'),
-                subtitle: const Text('欢迎访问'),
+                subtitle: const Text('个人网页'),
                 onTap: () {
                   launchUrl(
                     Uri.parse('https://4evergr8.github.io'),
@@ -73,49 +84,55 @@ class AboutPage extends StatelessWidget {
               ),
               const SizedBox(height: 8), // 添加段落间距
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start, // 设置为左对齐
                 children: [
                   Text(
-                    '源代码作者为KimiAI和ChatGPT，感谢二位开发者的付出。',
+                    '   源代码作者为KimiAI和ChatGPT，感谢二位开发者的付出。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    '此软件使用IntelliJ IDEA作为编译器。',
+                    '   此软件使用IntelliJ IDEA作为编译器。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    'GithubCopilot插件彻底干亖了Cursor，免费，还有三种模式。',
+                    '   GithubCopilot插件彻底干亖了Cursor，免费，还有三种模式。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    '软件使用了Flutter框架和Dart语言。',
+                    '   软件使用了Flutter框架和Dart语言。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    '本地搜图功能借助了CloudflareWorker和R2存储桶。',
+                    '   本地搜图功能借助了CloudflareWorker和R2存储桶。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    '在线搜图功能灵感来源于搜图Bot酱。',
+                    '   在线搜图功能灵感来源于搜图Bot酱。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    'OCR功能来自google_mlkit_text_recognition。',
+                    '   OCR功能来自google_mlkit_text_recognition。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    '语言检测功能来自google_mlkit_language_id。',
+                    '   语言检测功能来自google_mlkit_language_id。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   const SizedBox(height: 8), // 添加段落间距
                   Text(
-                    '离线翻译功能来自google_mlkit_translation。',
+                    '   离线翻译功能来自google_mlkit_translation。',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 8), // 添加段落间距
+                  Text(
+                    '   软件图标和我的头像均使用GoogleEmojiKitchen制作。',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                 ],
