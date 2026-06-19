5703e3e8ebbaa2d5affbea61690ec1711017c31a
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-19 20:53:10 +0800

    Update internet_page.dart

--- lib/views/internet_page.dart
+++ lib/views/internet_page.dart
@@ -72,7 +72,7 @@ class _InternetPageState extends State<InternetPage> {
   void _handleAdThenNavigate(VoidCallback action) {
     _adStateIndex++;
 
-    if (_adStateIndex % 3 == 0 && _isAdReady && _interstitialAd != null) {
+    if (_adStateIndex % 3 == 1 && _isAdReady && _interstitialAd != null) {
       _pendingAction = action;
       _interstitialAd!.show();
     } else {
