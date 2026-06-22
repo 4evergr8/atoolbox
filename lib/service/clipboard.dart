829d1928be8bd192a847880ec813aebdc26f0705
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-22 15:25:04 +0800

    Update clipboard.dart

--- lib/service/clipboard.dart
+++ lib/service/clipboard.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/services.dart';
 
-Future<void> clipboard_copy(String text) async {
+Future<void> clipboardCopy(String text) async {
   await Clipboard.setData(ClipboardData(text: text));
 }
 
-Future<String> clipboard_paste() async {
+Future<String> clipboardPaste() async {
   ClipboardData? data = await Clipboard.getData(Clipboard.kTextPlain);
   return data?.text ?? '';
 }
