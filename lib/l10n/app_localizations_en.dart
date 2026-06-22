ec552ea19a9f1068bac35e8bfb535de7f4fc090d
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-22 20:09:04 +0800

    Update app_localizations_en.dart

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -1,6 +1,5 @@
 // ignore: unused_import
 import 'package:intl/intl.dart' as intl;
-
 import 'app_localizations.dart';
 
 // ignore_for_file: type=lint
@@ -164,4 +163,7 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String get waiting => '请稍候...';
+
+  @override
+  String get success_video => '视频备份完成';
 }
