4a9905c0a707709a065268894d1c89d7e2bd789f
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-26 01:01:40 +0800

    Update qrcode.dart

--- lib/service/qrcode.dart
+++ lib/service/qrcode.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
-import 'package:picorigin/widget.dart';
 
 Future<String> scanQRCodeFromImage(BuildContext context, String filePath) async {
   final inputImage = InputImage.fromFilePath(filePath);
   final formats = [BarcodeFormat.all];
   final barcodeScanner = BarcodeScanner(formats: formats);
 
-    final List<Barcode> barcodes = await barcodeScanner.processImage(inputImage);
+  final List<Barcode> barcodes = await barcodeScanner.processImage(inputImage);
 
-    await barcodeScanner.close();
+  await barcodeScanner.close();
 
+  // 使用换行符连接所有二维码内容
+  final String result = barcodes.map((barcode) => barcode.rawValue.toString()).join('\n');
 
-    // 使用换行符连接所有二维码内容
-    final String result = barcodes.map((barcode) => barcode.rawValue.toString()).join('\n');
-
-    return result;
-
+  return result;
 }
