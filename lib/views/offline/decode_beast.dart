866d42751161e89af03b82ad9c240c2690257bc9
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-26 00:45:35 +0800

    Update decode_beast.dart

--- lib/views/offline/decode_beast.dart
+++ lib/views/offline/decode_beast.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:picorigin/l10n/app_localizations.dart';
 import 'package:picorigin/service/decode.dart';
 import 'package:picorigin/widget.dart';
 
@@ -11,27 +12,23 @@ class BeastEncodeDecode extends StatefulWidget {
 }
 
 class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
-  final TextEditingController _cipherController = TextEditingController(text: '理的说的说说说道说说理道的理道理的的说道'); // 密文
-  final TextEditingController _plainController = TextEditingController(text: '你好'); // 明文
-  final TextEditingController _dictController = TextEditingController(text: '说的道理'); // 字典
+  final TextEditingController _cipherController = TextEditingController(text: ''); // 密文
+  final TextEditingController _plainController = TextEditingController(text: ''); // 明文
+  final TextEditingController _dictController = TextEditingController(text: ''); // 字典
 
   // 加密并复制
   void _encryptAndCopy() async {
     String dict = _dictController.text;
     String plain = _plainController.text;
 
-    if (dict.length != 4) {
-      showSnackBarGlobal("fail", '字典必须为4个字符');
-      return;
-    }
 
     try {
       String cipher = await encodeBeast(dict, plain);
       _cipherController.text = cipher;
       await Clipboard.setData(ClipboardData(text: cipher));
-      showSnackBarGlobal("success", '已复制到剪贴板');
+      showSnackBarGlobal("success",AppLocalizations.of(context)!.copied);
     } catch (e) {
-      showSnackBarGlobal("fail", '$e');
+      showSnackBarGlobal("error", '$e');
     }
   }
 
@@ -44,9 +41,9 @@ class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
       String plain = await decodeBeast(cipher, dict);
       _plainController.text = plain;
       await Clipboard.setData(ClipboardData(text: plain));
-      showSnackBarGlobal("success", '已复制到剪贴板');
+      showSnackBarGlobal("success", AppLocalizations.of(context)!.copied);
     } catch (e) {
-      showSnackBarGlobal("fail", '$e');
+      showSnackBarGlobal("error", '$e');
     }
   }
 
@@ -72,7 +69,7 @@ class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('兽音加解密工具', style: theme.textTheme.headlineMedium),
+        title: Text(AppLocalizations.of(context)!.beast, style: theme.textTheme.headlineMedium),
         backgroundColor: theme.colorScheme.inversePrimary,
       ),
       body: SingleChildScrollView(
@@ -84,10 +81,10 @@ class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
             _buildSettingCard(
               context,
               icon: Icons.view_array,
-              title: '字典（4字符）',
+              title:AppLocalizations.of(context)!. dict,
               child: TextField(
                 controller: _dictController,
-                decoration: const InputDecoration(labelText: '请输入字典', hintText: '说的道理'),
+                decoration:  InputDecoration(labelText: AppLocalizations.of(context)!.dict_enter),
                 maxLength: 4,
               ),
             ),
@@ -97,10 +94,10 @@ class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
             _buildSettingCard(
               context,
               icon: Icons.password,
-              title: '密文',
+              title: AppLocalizations.of(context)!.decode,
               child: TextField(
                 controller: _cipherController,
-                decoration: const InputDecoration(labelText: '请输入密文', hintText: '你好'),
+                decoration:  InputDecoration(labelText: AppLocalizations.of(context)!.decode_enter),
                 maxLines: null,
                 minLines: 3,
               ),
@@ -113,13 +110,13 @@ class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
                 ElevatedButton.icon(
                   onPressed: _decryptAndCopy,
                   icon: const Icon(Icons.content_copy),
-                  label: const Text('解密并复制'),
+                  label: Text(AppLocalizations.of(context)!.decode_copy),
                 ),
                 const SizedBox(width: 12),
                 ElevatedButton.icon(
                   onPressed: _pasteToCipher,
                   icon: const Icon(Icons.assignment_returned),
-                  label: const Text('粘贴'),
+                  label: Text(AppLocalizations.of(context)!.paste),
                 ),
               ],
             ),
@@ -130,10 +127,10 @@ class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
             _buildSettingCard(
               context,
               icon: Icons.text_fields,
-              title: '明文',
+              title: AppLocalizations.of(context)!.encode,
               child: TextField(
                 controller: _plainController,
-                decoration: const InputDecoration(labelText: '请输入明文', hintText: '你好'),
+                decoration:  InputDecoration(labelText: AppLocalizations.of(context)!.encode_enter),
                 maxLines: null,
                 minLines: 3,
               ),
@@ -144,13 +141,13 @@ class _BeastEncodeDecodeState extends State<BeastEncodeDecode> {
                 ElevatedButton.icon(
                   onPressed: _encryptAndCopy,
                   icon: const Icon(Icons.content_copy),
-                  label: const Text('加密并复制'),
+                  label: Text(AppLocalizations.of(context)!.encode_copy),
                 ),
                 const SizedBox(width: 12),
                 ElevatedButton.icon(
                   onPressed: _pasteToPlain,
                   icon: const Icon(Icons.assignment_returned),
-                  label: const Text('粘贴'),
+                  label: Text(AppLocalizations.of(context)!.paste),
                 ),
               ],
             ),
