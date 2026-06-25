d91126962e19466c28b6e99c8531e2370041ad2b
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-26 00:45:53 +0800

    Update qrcode.dart

--- lib/service/qrcode.dart
+++ lib/service/qrcode.dart
@@ -6,22 +6,15 @@ Future<String> scanQRCodeFromImage(BuildContext context, String filePath) async 
   final inputImage = InputImage.fromFilePath(filePath);
   final formats = [BarcodeFormat.all];
   final barcodeScanner = BarcodeScanner(formats: formats);
-  try {
+
     final List<Barcode> barcodes = await barcodeScanner.processImage(inputImage);
 
     await barcodeScanner.close();
 
-    if (barcodes.isEmpty) {
-      showSnackBarGlobal("fail", '没有识别到二维码');
-      return '没有识别到二维码';
-    }
 
     // 使用换行符连接所有二维码内容
     final String result = barcodes.map((barcode) => barcode.rawValue.toString()).join('\n');
 
     return result;
-  } catch (e) {
-    showSnackBarGlobal("fail", '$e');
-    return '二维码识别失败: $e';
-  }
+
 }
